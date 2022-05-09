f590f57a7f6a6d030817d2346f4fbaf56b888a09
Kaisha Meichell Gerhardt <70034111+KaishaGerhardt@users.noreply.github.com>
2022-05-09 15:10:52 -0600

    Add files via upload

---
+++ WeeklyFun/ImageMovie.js
@@ -0,0 +1,30 @@
+
+var mainColor =  180;
+
+function setup() {
+    createCanvas(640, 480);
+    background(120);
+    frameRate(10);
+    noStroke();
+    rectMode(CENTER);
+}
+function draw() {
+    fill(random(mainColor),random(mainColor),random(mainColor), random(100));
+
+    var size= random(200);
+    var size2= random(200);
+    ellipse(random(width), random(height), size, size2);
+
+    size= random(200);
+    size2= random(200);
+    arc(random(width), random(height), size,size2, 0, HALF_PI);
+
+    if (frameCount % 1 == 2) {
+        mainColor = 255 - mainColor; // 255 0 255 0 255 0 ..
+    }
+    //saveFrames("myMovie",".png",1,25);
+
+    if (frameCount > 10) { // 2 second * 25 fps = 25
+        //noLoop();
+    }
+}
